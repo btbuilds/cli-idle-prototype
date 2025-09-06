bee9169cd70a096abb51d5d257fa81a749a96e36
btbuilds <bthompson8807@gmail.com>
2025-09-06 11:45:12 -0400

    Added max ticket number tracking

--- src/core/storage.py
+++ src/core/storage.py
@@ -1,6 +1,5 @@
 import json
 from pathlib import Path
-from models import Ticket, Customer, Technician
 
 DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 
@@ -8,11 +7,14 @@ CUSTOMERS_FILE = DATA_DIR / "customers.json"
 TICKETS_FILE = DATA_DIR / "tickets.json"
 TECHS_FILE = DATA_DIR / "technicians.json"
 
+# File map for the json data
 FILE_MAP = {
     'customers': CUSTOMERS_FILE,
     'tickets': TICKETS_FILE,
     'technicians': TECHS_FILE
 }
+
+COUNTER_FILE = DATA_DIR / "maxticket.txt"
 
 def initialize_files():
     DATA_DIR.mkdir(parents=True, exist_ok=True)
@@ -20,6 +22,9 @@ def initialize_files():
     for data_type in FILE_MAP:
         if not FILE_MAP[data_type].exists():
             save_json(FILE_MAP[data_type], [])
+    if not COUNTER_FILE.exists():
+        with COUNTER_FILE.open("w", encoding="utf-8") as f:
+            f.write("0")
 
 
 def load_json(path: Path) -> list[dict]:
@@ -37,3 +42,14 @@ def load_data(data_type: str) -> list[dict]:
 
 def save_data(data_type: str, data: list[dict]) -> None:
     save_json(FILE_MAP[data_type], data)
+
+def get_next_ticket_number():
+    with open(COUNTER_FILE, 'r') as f:
+        current_max = int(f.read().strip())
+        
+    next_number = current_max + 1
+    
+    with open(COUNTER_FILE, 'w') as f:
+        f.write(str(next_number))
+    
+    return next_number
