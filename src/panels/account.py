994308571d25a2c8e8ac1c2c006f365e87f2ff51
btbuilds <bthompson8807@gmail.com>
2025-09-30 10:50:38 -0400

    Move login/logout handling from sidebar to account panel.

---
+++ src/panels/account.py
@@ -0,0 +1,39 @@
+from textual import on
+from textual.app import ComposeResult
+from textual.widgets import Input, Button, Label
+from textual.containers import Vertical
+from panels.base_screen import BaseScreen
+
+class AccountScreen(BaseScreen):
+    BINDINGS = [("escape", "app.pop_screen", "Close screen")]
+
+    def compose(self) -> ComposeResult:
+        yield from super().compose() # This gets header/sidebar/footer
+        with Vertical(id="login-content"):
+            yield Button("Login", id="login", variant="primary")
+            yield Button("Logout", id="logout", variant="primary", disabled=True)
+        
+
+    def on_mount(self) -> None:
+        self.check_button_state()
+    
+    @on(Button.Pressed, "#login")
+    def push_login(self):
+        from panels.login import LoginScreen # Import here to avoid circular import issue.
+        # Comment on next line ignores pylance/vscode error since the code works
+        self.screen.show_sidebar = False  # type: ignore[attr-defined]
+        self.app.pop_screen()
+        self.app.push_screen(LoginScreen())
+    
+    @on(Button.Pressed, "#logout")
+    def logout(self):
+        self.app.logout_user()
+        self.check_button_state()
+
+    def check_button_state(self):
+        if self.app.current_technician:
+            self.query_one("#login", Button).disabled = True
+            self.query_one("#logout", Button).disabled = False
+        else:
+            self.query_one("#login", Button).disabled = False
+            self.query_one("#logout", Button).disabled = True
--- src/panels/sidebar.py
+++ src/panels/sidebar.py
@@ -9,18 +9,15 @@ class Sidebar(Widget):
         with Vertical(id="sidebar"):
             yield Button("New Ticket", id="new", variant="primary")
             yield Button("Edit Ticket", id="edit", variant="primary")
-            yield Button("Login", id="login", variant="success")
+            yield Button("Account", id="account", variant="success")
             yield Button("Exit", id="exit", variant="error")
     
-    @on(Button.Pressed, "#login")
-    def push_login(self) -> None:
-        from panels.login import LoginScreen # Import here to avoid circular import issue.
-        # don't push LoginScreen if we're already on it
-        if isinstance(self.screen, LoginScreen):
-            return
+    @on(Button.Pressed, "#account")
+    def push_account(self) -> None:
+        from panels.account import AccountScreen # Import here to avoid circular import issue.
         # Comment on next line ignores pylance/vscode error since the code works
         self.screen.show_sidebar = False  # type: ignore[attr-defined]
-        self.app.push_screen(LoginScreen())
+        self.app.push_screen(AccountScreen())
     
     @on(Button.Pressed, "#exit")
     def quit_button(self) -> None:
