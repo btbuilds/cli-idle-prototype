ee200dcc0822fa0b0eec6c395025322c621c2c11
btbuilds <bthompson8807@gmail.com>
2025-09-30 12:29:32 -0400

    Clean up commented test code.

--- src/panels/login.py
+++ src/panels/login.py
@@ -36,12 +36,6 @@ class LoginScreen(BaseScreen):
 
 class SuccessPopup(ModalScreen):
     def compose(self) -> ComposeResult:
-        # yield Grid(
-        #     Label("Are you sure you want to quit?", id="question"),
-        #     Button("Quit", variant="error", id="quit"),
-        #     Button("Cancel", variant="primary", id="cancel"),
-        #     id="dialog",
-        # )
         with Vertical(id="successpopup"):
             yield Label("Login successful!")
             yield Button("Close", id="close", variant="success")
