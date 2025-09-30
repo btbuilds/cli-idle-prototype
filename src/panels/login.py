6ce2deb380b9657e1428814b10d5a877c09ce18d
btbuilds <bthompson8807@gmail.com>
2025-09-30 12:21:02 -0400

    Add a modal popup screen on successful login.

--- src/panels/login.py
+++ src/panels/login.py
@@ -1,7 +1,8 @@
 from textual import on
 from textual.app import ComposeResult
 from textual.widgets import Input, Button, Label
-from textual.containers import Vertical
+from textual.containers import Vertical, Grid
+from textual.screen import ModalScreen
 from panels.base_screen import BaseScreen
 
 class LoginScreen(BaseScreen):
@@ -23,9 +24,7 @@ class LoginScreen(BaseScreen):
         
         if tech and tech.is_active:
             self.app.login_user(tech)  # Pass the Technician object
-            self.query_one("#status-label", Label).update(
-                "[bold green]Login successful!"
-            )
+            self.app.push_screen(SuccessPopup())
         elif tech and not tech.is_active:
             self.query_one("#status-label", Label).update(
                 "[bold red]Username is not active!"
@@ -34,3 +33,20 @@ class LoginScreen(BaseScreen):
             self.query_one("#status-label", Label).update(
                 "[bold red]Username does not exist!"
             )
+
+class SuccessPopup(ModalScreen):
+    def compose(self) -> ComposeResult:
+        # yield Grid(
+        #     Label("Are you sure you want to quit?", id="question"),
+        #     Button("Quit", variant="error", id="quit"),
+        #     Button("Cancel", variant="primary", id="cancel"),
+        #     id="dialog",
+        # )
+        with Vertical(id="successpopup"):
+            yield Label("Login successful!")
+            yield Button("Close", id="close", variant="success")
+    
+    @on(Button.Pressed, "#close")
+    def close_screen(self):
+        self.app.pop_screen()
+        self.app.pop_screen()
