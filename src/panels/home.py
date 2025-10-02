64d7918afddeedb3d72ec55c27ac1cbfad90bf82
btbuilds <bthompson8807@gmail.com>
2025-10-02 09:47:13 -0400

    Add ASCII art to home panel.

--- src/panels/home.py
+++ src/panels/home.py
@@ -1,8 +1,10 @@
 from textual.app import ComposeResult
 from textual.widgets import Static
+from art import text2art
 from panels.base_screen import BaseScreen
 
 class HomeScreen(BaseScreen):
     def compose(self) -> ComposeResult:
         yield from super().compose()  # This gets header/sidebar/footer
-        yield Static("Home Content", id="main-content")
+        logo = str(text2art("Ticket RPG", font="cybermedium")) # This already returns a string, but pylance complains about it otherwise.
+        yield Static(content=logo, id="main-content")
